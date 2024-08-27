bb9b00e8a8914a2e92a5fad9d39af6c065d0de60
kier-ious <kier.diemer@gmail.com>
2024-08-27 15:03:55 -0500

    cloned J's repo and pushing to mine

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import SchoolCatalog from "./SchoolCatalog";
 import Header from "./Header";
 import ClassSchedule from "./ClassSchedule";
+// adding comment to push
 
 export default function App() {
   return (
