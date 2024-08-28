951505977df00235ae97313cef2d1d25c1a4ad6c
kier-ious <kier.diemer@gmail.com>
2024-08-28 15:48:29 -0500

    completed task2, filtering the data in the api handles case sensitivity

--- src/SchoolCatalog.jsx
+++ src/SchoolCatalog.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 
-// Custom hook
+// Custom hook for fetching data from API
 function useFetch(url) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -29,14 +29,40 @@ function useFetch(url) {
 
 function SchoolCatalog() {
   const { data: courses, loading, error } = useFetch('/api/courses.json');
+  const [search, setSearch] = useState('');
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error loading courses: {error.message}</div>;
 
+  // Filter courses by user input
+  const filteredCourses = courses.filter(course => {
+    // Convert all searches to lowercase for comparison
+    const searchTerm = search.toLowerCase().trim();
+    const courseNumber = course.courseNumber.toLowerCase();
+    const courseName = course.courseName.toLowerCase();
+
+    console.log('Searching for:', searchTerm) ||
+    console.log('Course:', courseNumber, courseName);
+
+    // Check if course # and course name contains searched user input
+    return (
+      course.courseNumber.toLowerCase().includes(searchTerm) ||
+      course.courseName.toLowerCase().includes(searchTerm)
+    );
+  });
+
+  console.log('Filtered courses:', filteredCourses);
+
   return (
     <div className="school-catalog">
       <h1>School Catalog</h1>
-      <input type="text" placeholder="Search" />
+      <input
+        type="text"
+        placeholder="Search"
+        value={search}
+        // onChange will update the search state
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <table>
         <thead>
           <tr>
@@ -49,12 +75,12 @@ function SchoolCatalog() {
           </tr>
         </thead>
         <tbody>
-          {courses.length === 0 ? (
+          {filteredCourses.length === 0 ? (
             <tr>
               <td colSpan="6">No Course Available</td>
             </tr>
           ) : (
-            courses.map((course, index) => (
+            filteredCourses.map((course, index) => (
               <tr key={index}>
                 <td>{course.trimester}</td>
                 <td>{course.courseNumber}</td>
